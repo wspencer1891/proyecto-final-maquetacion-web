d619f001d400b6db8f881d1d673ccd6f0ddd51fd
wspencer1891 <walterspencer10@gmail.com>
2024-09-30 13:36:23 -0300

    Esta rama y este commit tienen agregado JS

---
+++ script.js
@@ -0,0 +1,20 @@
+const menu = document.querySelector('#menu-icon');
+const navList = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x'); // Cambiar el ícono del menú cuando se hace clic
+    navList.classList.toggle('open'); // Abrir/cerrar el menú
+};
+
+const sr = ScrollReveal({
+    distance: '65px',
+    duration: 2000,
+    delay: 450,
+    reset: true
+});
+sr.reveal('.hero-text', { delay: 200, origin: 'top' });
+sr.reveal('.hero-img', { delay: 450, origin: 'top' });
+sr.reveal('.icons', { delay: 500, origin: 'left' });
+sr.reveal('.scroll-down', { delay: 450, origin: 'left' });
+
+
